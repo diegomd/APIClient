6cc41d30b296633cdc664d06c1eebfc0aa9680c2
frmaia <br.frmaia@gmail.com>
2012-09-05 11:40:35 -0300

    Tratamento de erros

--- Playground/app/controllers/Playground.java
+++ Playground/app/controllers/Playground.java
@@ -4,13 +4,15 @@ import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import shared.Utils;
-import views.html.playground;
 
 import com.sambatech.apiclient.LiquidAPIClient;
+import com.sambatech.apiclient.exception.ParserException;
+import com.sambatech.apiclient.exception.RequestException;
 import com.sambatech.apiclient.filter.APIFilter;
 import com.sambatech.apiclient.filter.OrderBy;
 import com.sambatech.apiclient.filter.Sort;
-import com.sambatech.apiclient.response.Medias;
+import com.sambatech.apiclient.http.HttpRequest;
+import views.html.playground;
 
 public class Playground extends Controller {
 	
@@ -19,24 +21,47 @@ public class Playground extends Controller {
 	 ****************************/
 	public static Result index() {
 		
-		APIRequest request = getForm(APIRequest.class);
+		APIRequest apiRequest = getForm(APIRequest.class);
 		
-		if (request.apikey == null) {
+		if (apiRequest.apikey == null) {
 			return ok(playground.render("...","..."));
 		}
+		
+		APIFilter apiFilter = getAPIFilter(apiRequest);
+
+		LiquidAPIClient liquidAPIClient = new LiquidAPIClient(apiRequest.apikey);
+		HttpRequest httpRequest;
+		try {
+			httpRequest = liquidAPIClient.getMediasRequest(apiFilter, true);
+			String responseBody = httpRequest.getResponseBody();
+			String requestedUrl = httpRequest.getUrl();
+			responseBody = Utils.transformXML(responseBody);
+			System.out.println(responseBody);
+			return ok(playground.render(requestedUrl, responseBody));
+
+		} catch (RequestException e) {
+			return ok(playground.render(e.getHttpRequest().getUrl(),e.getHttpRequest().getResponseBody()));
+		} catch (ParserException e) {
+			return ok(playground.render("...", "Sorry, an error has ocurred while trying to get response."));
+		}		
+	}
+	
+	public static void main (String [] args) {
+		/*
+		System.out.println(Utils.transformXML("<teste><object>bla</object></teste>"));		
+		
+		APIRequest request = new APIRequest();
+		request.apikey = "18a3dcacfff63aca5dd93594eb658098";
+		request.limit = 1;
 		
 		APIFilter apiFilter = getAPIFilter(request);
 
 		LiquidAPIClient liquidAPIClient = new LiquidAPIClient(request.apikey);
 		Medias medias = liquidAPIClient.getMedias(apiFilter);
 		
-		//TODO test headers to avoid null pointer
-		String requestedUrl = medias.getHttpResponse().getUrl();
-		String responseBody = medias.getHttpResponse().getResponseBody();
-		
+		String responseBody = medias.getHttpResponse().getResponseBody();		
 		System.out.println(Utils.transformXML(responseBody));
-		
-		return ok(playground.render(requestedUrl, Utils.transformXML(responseBody)));
+		*/
 	}
 	
 
@@ -106,7 +131,7 @@ public class Playground extends Controller {
 		
 		
 		if (request.sort != null && request.sort.length() > 0)
-			apiFilter.setSort(Sort.valueOf(request.sort));
+			apiFilter.setSort(Sort.ASC);
 		
 		
 		return apiFilter;
