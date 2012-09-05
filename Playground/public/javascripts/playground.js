43c28fdf8597236d9bee00651b6b65aee066a9ce
frmaia <br.frmaia@gmail.com>
2012-09-05 19:33:22 -0300

    Validacao de campos obrigatórios.

--- Playground/public/javascripts/playground.js
+++ Playground/public/javascripts/playground.js
@@ -1,3 +1,6 @@
+
+
+
 var	showJavaRequest = function () {
 	$("#request_html").hide();
 	$("#request_java").show();
@@ -14,27 +17,66 @@ var showHtmlRequest = function () {
 	$("#request_java_li").removeClass("active");
 }
 	
+var requiredFields = [];
+var isFormValid = function () {
+	
+	if ( $("#apikey").val().length == 0 ) {
+		return false;
+	}
+	
+	for(i=0;i<requiredFields.length;i++){
+		if ($("#"+requiredFields[i]).find("input").val().length == 0) {
+			return false;
+		}
+	}
+	
+	return true;
+	
+}
 	
 $("#request_java_link").live('click', showJavaRequest);
 
 $("#request_html_link").live('click', showHtmlRequest);
 
 $("#btn-get").live('click', function(){
+	if (!isFormValid()) {
+		alert("Sorry, is not possible to execute this action. Please check if the required fields are filled.");
+		return;
+	}
+	
 	$("#method").val("GET");
 	$("#form").submit();
 });
 
+
+
+
 $("#btn-put").live('click', function(){
+	if (!isFormValid()) {
+		alert("Sorry, is not possible to execute this action. Please check if the required fields are filled.");
+		return;
+	}
+
 	$("#method").val("PUT");
 	$("#form").submit();
 });
 
 $("#btn-post").live('click', function(){
+	if (!isFormValid()) {
+		alert("Sorry, is not possible to execute this action. Please check if the required fields are filled.");
+		return;
+	}
+
 	$("#method").val("POST");
 	$("#form").submit();
 });
 
 $("#btn-delete").live('click', function(){
+	if (!isFormValid()) {
+		alert("Sorry, is not possible to execute this action. Please check if the required fields are filled.");
+		return;
+	}
+
 	$("#method").val("DELETE");
 	$("#form").submit();
 });
@@ -43,6 +85,12 @@ $("#btn-delete").live('click', function(){
 function showElements(ArrayOfElementsId){
 	for(i=0;i<ArrayOfElementsId.length;i++){
 		$("#"+ArrayOfElementsId[i]).show();
+	}	
+}
+
+function setDivLabelAsRequired(ArrayOfElementsId) {
+	for(i=0;i<ArrayOfElementsId.length;i++){
+		$("#"+ArrayOfElementsId[i]).find("label").addClass("required");
 	}	
 }
 
@@ -56,6 +104,13 @@ var showOnlyFields = function (fieldArray) {
 	showElements(fieldArray);
 }
 
+var defineRequiredFields = function (fieldArray) {
+	$("[id$='field']").find("label").removeClass("required");
+	setDivLabelAsRequired(fieldArray);	
+}
+
+
+
 var configureEndPoint = function (endpointUri) {
 
 	$(".endpoint-li").removeClass("active");
@@ -68,139 +123,163 @@ var configureEndPoint = function (endpointUri) {
 			$("#li-endpoint-medias").addClass("active");
 			showOnlyFields([ "search-field", "first-field", "limit-field", "filter-field", "orderby-field", "sort-field", "recursivechannel-field"]);
 			showOnlyButtons([ "btn-get"]);
+			requiredFields = [];
 			break;
 
 		case "MEDIAS_COUNT":
 			$("#li-endpoint-medias-count").addClass("active"); 
 			showOnlyFields([]);
 			showOnlyButtons([ "btn-get"]);
+			requiredFields = [];
 			break;
 
 		case "MEDIAS_RATINGS":
 			$("#li-endpoint-medias-ratings").addClass("active"); 
 			showOnlyFields([ "first-field", "limit-field", "lastmodified-field", "filter-field"]);
 			showOnlyButtons([ "btn-get"]);
-			
+			requiredFields = [];
 			break;			
 			
 		case "MEDIAS_VIEWS":
 			$("#li-endpoint-medias-views").addClass("active"); 
 			showOnlyFields([ "first-field", "limit-field", "lastmodified-field"]);
 			showOnlyButtons([ "btn-get"]);
+			requiredFields = [];
 			break;
 			
 		case "MEDIAS_MEDIAID":
 			$("#li-endpoint-medias-mediaid").addClass("active"); 
 			showOnlyFields([ "filter-field", "mediaid-field"]);
 			showOnlyButtons([ "btn-get", "btn-put", "btn-delete" ]);
+			requiredFields = [ "mediaid-field" ];
 			break;
 
 		case "MEDIAS_URLS_MEDIAFILEID":
 			$("#li-endpoint-medias-urls-mediafileid").addClass("active");
 			showOnlyFields([ "mediafileid-field"]);
 			showOnlyButtons([ "btn-get"]);
+			defineRequiredFields([ "mediafileid-field" ]);
+			requiredFields = [ "mediafileid-field" ];
 			break;
 
 		case "MEDIAS_VIEWS_MEDIAFILEID":
 			$("#li-endpoint-medias-views-mediafileid").addClass("active");
 			showOnlyFields([ "mediafileid-field", "sessionid-field", "quarter-field" ]);
 			showOnlyButtons([ "btn-post"]);
+			requiredFields = [ "mediafileid-field", "sessionid-field" ];
 			break;
 			
 		case "MEDIAS_MEDIAID_RATING":
 			$("#li-endpoint-medias-mediaid-rating").addClass("active");
 			showOnlyFields([ "mediaid-field", "filter-field"]);
 			showOnlyButtons([ "btn-get", "btn-post" ]);
+			requiredFields = [ "mediaid-field" ];
 			break;
 
 		case "MEDIAS_MEDIAID_RELATED":
 			$("#li-endpoint-medias-mediaid-related").addClass("active");
 			showOnlyFields([ "mediaid-field", "first-field", "limit-field", "filter-field"]);
 			showOnlyButtons([ "btn-get"]);
+			requiredFields = [ "mediaid-field" ];
 			break;
 			
 		case "MEDIAS_MEDIAID_THUMBS":
 			$("#li-endpoint-medias-mediaid-thumbs").addClass("active");
 			showOnlyFields([ "mediaid-field", "filter-field" ]);
 			showOnlyButtons([ "btn-get"]);
+			requiredFields = [ "mediaid-field" ];
 			break;
 
 		case "MEDIAS_MEDIAID_VIEWS":
 			$("#li-endpoint-medias-mediaid-views").addClass("active");
 			showOnlyFields([ "mediaid-field"]);
 			showOnlyButtons([ "btn-get"]);
+			requiredFields = [ "mediaid-field" ];
 			break;
 
 		case "MEDIAS_URLS_MEDIAID_OUTPUTNAME":
 			$("#li-endpoint-medias-urls-mediaid-outputname").addClass("active");
 			showOnlyFields([ "mediaid-field", "outputname-field" ]);
 			showOnlyButtons([ "btn-get"]);
+			requiredFields = [ "mediaid-field", "outputname-field" ];
 			break;
 
 		case "MEDIAS_MEDIAID_VIEWS_OUTPUTNAME":
 			$("#li-endpoint-medias-mediaid-views-outputname").addClass("active");
 			showOnlyFields([ "mediaid-field", "outputname-field", "sessionid-field", "quarter-field" ]);
 			showOnlyButtons([ "btn-get"]);
+			requiredFields = [ "mediaid-field", "outputname-field", "sessionid-field" ];
 			break;	
 			
 		case "CHANNELS":
 			$("#li-endpoint-channels").addClass("active");
 			showOnlyFields([ "filter-field", "limit-field", "first-field"]);
 			showOnlyButtons([ "btn-get"]);
+			requiredFields = [ ];
 			break;
 			
 		case "CHANNELS_COUNT":
 			$("#li-endpoint-channels-count").addClass("active");
 			showOnlyFields([ ]);
 			showOnlyButtons([ "btn-get"]);
+			requiredFields = [ ];
 			break;			
 			
 		case "CHANNELS_CHANNELID":
 			$("#li-endpoint-channels-channelid").addClass("active");
 			showOnlyFields([ "filter-field", "channelid-field"]);
 			showOnlyButtons([ "btn-get", "btn-put" ]);
+			requiredFields = [ "channelid-field" ];
 			break;
 
 		case "OUTPUTS":
 			$("#li-endpoint-outputs").addClass("active");
 			showOnlyFields([ ]);
 			showOnlyButtons([ "btn-get" ]);
+			requiredFields = [ ];
 			break;
 
 		case "OUTPUTS_COUNT":
 			$("#li-endpoint-outputs-count").addClass("active");
 			showOnlyFields([ ]);
 			showOnlyButtons([ "btn-get" ]);
+			requiredFields = [ ];
 			break;
 
 		case "REPORT_DETAIL_TRAFFIC":
 			$("#li-endpoint-report-detail-traffic").addClass("active");
 			showOnlyFields([ "begin-field", "end-field" ]);
 			showOnlyButtons([ "btn-get" ]);
+			requiredFields = [ ];
 			break;
 
 		case "REPORT_DETAIL_MEDIAS":
 			$("#li-endpoint-report-detail-medias").addClass("active");
 			showOnlyFields([ "date-field"]);
 			showOnlyButtons([ "btn-get" ]);
+			requiredFields = [ ];
 			break;
 
 		case "REPORT_DETAIL_VIEWS":
 			$("#li-endpoint-report-detail-views").addClass("active");
 			showOnlyFields([ "begin-field", "end-field" ]);
 			showOnlyButtons([ "btn-get" ]);
+			requiredFields = [ ];
 			break;
 
 		case "REPORT_DETAIL_STORAGE":
 			$("#li-endpoint-report-detail-storage").addClass("active");
 			showOnlyFields([ "date-field" ]);
 			showOnlyButtons([ "btn-get" ]);
+			requiredFields = [ ];
 			break;			
 			
 		default:
 			configureEndPoint("MEDIAS");
 
 	}
+	
+	defineRequiredFields(requiredFields);
 
 }
 
